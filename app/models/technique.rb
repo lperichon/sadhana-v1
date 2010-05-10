b7d08c432190f328f5b0645f3452792574de79af
Luis Perichon <luigi@luigi-laptop.(none)>
2010-05-10 12:28:38 -0300

    missing.png

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,7 +1,5 @@
-require 'open-uri'
-
 class Technique < ActiveRecord::Base
   belongs_to :technique_type
-  has_attached_file :photo, :styles => { :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :thumb => "100x100>"}, :default_url => "/images/missing_:style.png"
   validates_presence_of :name, :technique_type
 end
