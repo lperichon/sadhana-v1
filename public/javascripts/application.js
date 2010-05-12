05fce2fa2d60be9c03cceac09da9223986d6074b
Luis Perichon <luigi@luigi-laptop.(none)>
2010-05-12 19:02:46 -0300

    use cufon

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     $('.help').live('mouseover', function() { $(this).addClass('ui-state-hover');});
     $('.help').live('mouseout', function() { $(this).removeClass('ui-state-hover');});
+
+    Cufon.replace('h1');
 });
