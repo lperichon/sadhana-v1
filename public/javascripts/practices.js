76080a11ae1a70851fb21d6bbbf374873cd34fed
Luis Perichon <luigi@luigi-laptop.(none)>
2010-05-12 16:09:35 -0300

    help tooltips

--- public/javascripts/practices.js
+++ public/javascripts/practices.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+  $('.practice_technique .actions li').live('mouseover', function() { $(this).addClass('ui-state-hover');});
+  $('.practice_technique .actions li').live('mouseout', function() { $(this).removeClass('ui-state-hover');});
+});
+
 var Practices = {
   accordion_draggable: function() {
     $('#techniques_accordion .asana').draggable({
