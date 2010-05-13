9bc7c50e44657e6a175fe2604d5384cd45639896
Luis Perichon <luigi@luigi-laptop.(none)>
2010-05-13 18:32:54 -0300

    even more translations

--- app/controllers/practice_events_controller.rb
+++ app/controllers/practice_events_controller.rb
@@ -16,7 +16,6 @@ class PracticeEventsController < UserApplicationController
     @practice_event = current_user.practice_events.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
       format.xml  { render :xml => @practice_event }
     end
   end
@@ -27,7 +26,6 @@ class PracticeEventsController < UserApplicationController
     @practice_event = current_user.practice_events.new(params[:practice_event])
 
     respond_to do |format|
-      format.html # new.html.erb
       format.xml  { render :xml => @practice_event }
       format.js {}
     end
@@ -45,11 +43,9 @@ class PracticeEventsController < UserApplicationController
 
     respond_to do |format|
       if @practice_event.save
-        format.html { redirect_to(@practice_event, :notice => 'Practice event was successfully created.') }
         format.xml  { render :xml => @practice_event, :status => :created, :location => @practice_event }
         format.js {}
       else
-        format.html { render :action => "new" }
         format.xml  { render :xml => @practice_event.errors, :status => :unprocessable_entity }
         format.js {}
       end
@@ -63,11 +59,9 @@ class PracticeEventsController < UserApplicationController
 
     respond_to do |format|
       if @practice_event.update_attributes(params[:practice_event])
-        format.html { redirect_to(@practice_event, :notice => 'Practice event was successfully updated.') }
         format.xml  { head :ok }
         format.js {}
       else
-        format.html { render :action => "edit" }
         format.xml  { render :xml => @practice_event.errors, :status => :unprocessable_entity }
         format.js {}
       end
@@ -81,7 +75,6 @@ class PracticeEventsController < UserApplicationController
     @practice_event.destroy
 
     respond_to do |format|
-      format.html { redirect_to(practice_events_url) }
       format.xml  { head :ok }
       format.json { render :json => :ok }
     end
