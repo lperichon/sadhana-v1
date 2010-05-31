1404474e1b1d91598ec5e2255f19f1780d1d4803
Luis Perichon <luigi@luigi-laptop.(none)>
2010-05-31 10:01:23 -0300

    preview technique dialog

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -1,9 +1,19 @@
 class TechniquesController < UserApplicationController
-  # GET /practices
-  # GET /practices.xml
+  # GET /techniques
+  # GET /techniques.xml
   def index
     @technique_type = TechniqueType.find_by_symbol(params[:symbol])
     @techniques = @technique_type.techniques
+
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  # GET /techniques/1
+  # GET /techniques/1.xml
+  def show
+    @technique = Technique.find(params[:id])
 
     respond_to do |format|
       format.js {}
