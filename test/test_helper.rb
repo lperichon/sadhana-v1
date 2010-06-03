e948f82ee5e65e9cb5574240747605df67aac7dd
Luis Perichon <luigi@luigi-laptop.(none)>
2010-06-03 10:06:22 -0300

    require devise test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,14 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  #fixtures :all
 
   # reset Sham before each test
   setup { Sham.reset }
 
   # Add more helper methods to be used by all tests here...
+
+  class ActionController::TestCase
+    include Devise::TestHelpers
+  end
 end
