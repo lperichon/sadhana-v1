e464bf0cf4ff6be78164cb3badf7adb7dd8dbdd9
Luis Perichon <luigi@luigi-laptop.(none)>
2010-06-07 11:10:45 -0300

    countdown

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
     $('.help').live('mouseover', function() { $(this).addClass('ui-state-hover');});
     $('.help').live('mouseout', function() { $(this).removeClass('ui-state-hover');});
-
-    Cufon.replace('h1');
 });
