d998f60aac3c23577ca64743cb67092d241ee250
Luis Perichon <luigi@luigi-laptop.(none)>
2010-06-10 17:32:42 -0300

    production host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Sadhana::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'http://sadhana.luisperichon.com.ar/' }
+
   # Enable threaded mode
   # config.threadsafe!
 end
