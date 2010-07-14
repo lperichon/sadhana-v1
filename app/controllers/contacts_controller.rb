3c8f696e10b5521b3307589c45f75eaa6a224058
Luis Perichon <luigi@luigi-laptop.(none)>
2010-07-14 11:10:00 -0300

    invite from Windows Live

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,11 @@
 class ContactsController < UserApplicationController
   layout 'popup'
+  skip_before_filter :verify_authenticity_token, :only => :create
   def index
     if params[:provider].present?
       provider = params[:provider].to_sym
       consumer = Contacts.new(provider)
       url = consumer.authentication_url('http://local.sadhana.com.ar/contacts')
-      
       session[:consumer] = consumer.serialize
       session[:provider] = provider
       redirect_to url
@@ -13,10 +13,20 @@ class ContactsController < UserApplicationController
       provider = session[:provider]
       consumer = Contacts.deserialize_consumer(provider, session[:consumer])
       if consumer.authorize(params)
+        Rails.logger.debug 'consumer authorized, listing contacts'
         @contacts = consumer.contacts
       else
         # handle error
       end
     end
+  end
+
+  # Windows Live posts to return url
+  def create
+    provider = session[:provider]
+    consumer = Contacts.deserialize_consumer(provider, session[:consumer])
+    @contacts = consumer.contacts(params)
+
+    render :action => :index
   end
 end
