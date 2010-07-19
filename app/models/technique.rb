e1ba32cf6202d1c778e099c0a388c3eccd4244f7
Luis Perichon <luigi@luigi-laptop.(none)>
2010-07-19 17:38:38 -0300

    s3

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,6 +1,10 @@
 class Technique < ActiveRecord::Base
   belongs_to :technique_type
-  has_attached_file :photo,:storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+  has_attached_file :photo,:storage => :s3,
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_ACCESS_KEY_ID'].to_s,
+                      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY'].to_s},
+                    :bucket => 'sadhana_development',
                     :styles => { :thumb => "100x100>"},
                     :default_url => "/images/missing_:style.png", :path => ":attachment/:id/:style/:filename"
   validates_presence_of :name, :technique_type
