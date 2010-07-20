49944b187fac4af1839decc4097385e652cd2dc8
Luis Perichon <luigi@luigi-laptop.(none)>
2010-07-20 13:32:08 -0300

    fix contacts config

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,2 +1,16 @@
-config = YAML.load_file("#{Rails.root}/config/contacts.yml")
-Contacts.configure(config[Rails.env])
+config = {
+  :google => {
+    :consumer_key => ENV['GOOGLE_CONSUMER_KEY'],
+    :consumer_secret => ENV['GOOGLE_CONSUMER_SECRET']
+  },
+  :yahoo => {
+    :consumer_key => ENV['YAHOO_CONSUMER_KEY'],
+    :consumer_secret => ENV['YAHOO_CONSUMER_SECRET']
+  },
+  :windows_live => {
+    :application_id => ENV['WINDOWSLIVE_APP_ID'],
+    :secret_key => ENV['WINDOWSLIVE_CONSUMER_KEY'],
+    :privacy_policy_url => ENV['WINDOWSLIVE_POLICY_URL']
+  }
+}
+Contacts.configure(config)
