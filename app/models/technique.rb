6667c6cc71d142aed3c0fa9e2bdd1c5c92221cc7
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-02 14:30:26 -0300

    new style for technique photos

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -5,7 +5,7 @@ class Technique < ActiveRecord::Base
                       :access_key_id => ENV['S3_ACCESS_KEY_ID'].to_s,
                       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY'].to_s},
                     :bucket => ENV['S3_BUCKET'].to_s,
-                    :styles => { :thumb => "100x100>"},
-                    :default_url => "/images/missing_:style.png", :path => ":attachment/:id/:style/:filename"
+                    :styles => { :thumb => "100x100>", :medium => "370x450>"},
+                    :default_url => "/images/missing_:style.png"
   validates_presence_of :name, :technique_type
 end
