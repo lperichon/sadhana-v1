bd72e2d202776a29a2daee25a1d3b0d0caf9c496
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-04 19:41:05 -0300

    parents

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,5 +1,7 @@
 class Technique < ActiveRecord::Base
   belongs_to :technique_type
+  belongs_to :parent, :class_name => "Technique", :foreign_key => "parent_id"
+  has_many :children, :class_name => 'Technique', :foreign_key => 'parent_id'
   has_attached_file :photo,:storage => :s3,
                     :s3_credentials => {
                       :access_key_id => ENV['S3_ACCESS_KEY_ID'].to_s,
@@ -8,4 +10,14 @@ class Technique < ActiveRecord::Base
                     :styles => { :thumb => "100x100>", :medium => "370x450>"},
                     :default_url => "/images/missing_:style.png", :path => ":attachment/:id/:style/:filename"
   validates_presence_of :name, :technique_type
+
+  def family_members
+    if self.parent.present?
+      self.parent.family_members
+    else
+      family_members = []
+      family_members << self
+      family_members << self.children
+    end
+  end
 end
