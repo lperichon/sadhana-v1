382dc9dbf964f223bafcd157bb98333cea3b6713
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-20 01:15:42 -0300

    fix bug on migration

--- db/migrate/20100819233301_migrate_primary_and_secondary_technique_types.rb
+++ db/migrate/20100819233301_migrate_primary_and_secondary_technique_types.rb
@@ -2,8 +2,10 @@ class MigratePrimaryAndSecondaryTechniqueTypes < ActiveRecord::Migration
   def self.up
     Part.all.each do |p|
       p.technique_type = TechniqueType.find_by_symbol(p.primary_technique_type.gsub(/\./,''))
-      p.secondary_technique_types.gsub(/\./, '').split(',').each do |symbol|
-        p.technique_types << TechniqueType.find_by_symbol(symbol.strip)
+      if p.secondary_technique_types
+        p.secondary_technique_types.gsub(/\./, '').split(',').each do |symbol|
+          p.technique_types << TechniqueType.find_by_symbol(symbol.strip)
+        end
       end
       p.save
     end
