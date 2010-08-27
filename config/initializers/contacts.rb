88ecdd4061c3ad5b1961f4682ba320617243c8a7
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-27 10:51:36 -0300

    load contacts.yml on development

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,16 +1,21 @@
-config = {
-  :google => {
-    :consumer_key => ENV['GOOGLE_CONSUMER_KEY'],
-    :consumer_secret => ENV['GOOGLE_CONSUMER_SECRET']
-  },
-  :yahoo => {
-    :consumer_key => ENV['YAHOO_CONSUMER_KEY'],
-    :consumer_secret => ENV['YAHOO_CONSUMER_SECRET']
-  },
-  :windows_live => {
-    :application_id => ENV['WINDOWSLIVE_APP_ID'],
-    :secret_key => ENV['WINDOWSLIVE_CONSUMER_KEY'],
-    :privacy_policy_url => ENV['WINDOWSLIVE_POLICY_URL']
+config = {}
+if Rails.env == 'development'
+  config = YAML.load_file("#{Rails.root}/config/contacts.yml")
+else
+  config = {
+    :google => {
+      :consumer_key => ENV['GOOGLE_CONSUMER_KEY'],
+      :consumer_secret => ENV['GOOGLE_CONSUMER_SECRET']
+    },
+    :yahoo => {
+      :consumer_key => ENV['YAHOO_CONSUMER_KEY'],
+      :consumer_secret => ENV['YAHOO_CONSUMER_SECRET']
+    },
+    :windows_live => {
+      :application_id => ENV['WINDOWSLIVE_APP_ID'],
+      :secret_key => ENV['WINDOWSLIVE_CONSUMER_KEY'],
+      :privacy_policy_url => ENV['WINDOWSLIVE_POLICY_URL']
+    }
   }
-}
+end
 Contacts.configure(config)
