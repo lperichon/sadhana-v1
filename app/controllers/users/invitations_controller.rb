0f6ed6b7322582149c7fc0615cfead61072894c2
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-30 12:08:07 -0300

    add to contacts on invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,7 +1,9 @@
 class Users::InvitationsController < Devise::InvitationsController
-  after_filter :invite_another_msg, :only => :create
+  after_filter :add_to_contacts, :only => :create
 
-  def invite_another_msg
+  def add_to_contacts
+    current_user.contacts << self.resource
+    current_user.save
     flash.now[:notice] << ' ' + t('users.invitations.invite_another', :new_invitation_link => self.class.helpers.link_to_function(t('users.invitations.invite_another_link'),"$('#new_invitation_dialog').dialog('open')"))
   end
 end
