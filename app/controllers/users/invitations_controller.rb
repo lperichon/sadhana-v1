c0f2c2538451a408aeeb3ec6ac72df941887abda
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-30 16:14:05 -0300

    add an existing contact

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,12 +1,15 @@
 class Users::InvitationsController < Devise::InvitationsController
-  after_filter :add_to_contacts, :only => :create
-
   # POST /resource/invitation
   def create
     self.resource = resource_class.send_invitation(params[resource_name])
 
     if resource.errors.empty?
       set_flash_message :notice, :send_instructions
+      add_to_contacts
+      redirect_to :back
+    elsif resource.errors[:email].present? && resource.id.present?
+      set_flash_message :notice, :user_exists
+      add_to_contacts
       redirect_to :back
     else
       render_with_scope :new
@@ -14,8 +17,10 @@ class Users::InvitationsController < Devise::InvitationsController
   end
 
   def add_to_contacts
-    current_user.contacts << self.resource
-    current_user.save
+    unless current_user.contacts.include?(self.resource)
+      current_user.contacts << self.resource
+      current_user.save
+    end
     flash.now[:notice] << ' ' + t('users.invitations.invite_another', :new_invitation_link => self.class.helpers.link_to_function(t('users.invitations.invite_another_link'),"$('#new_invitation_dialog').dialog('open')"))
   end
 end
