d1df3ecc362bae09b23b5b02dafffcadfc4d87ff
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-30 12:22:31 -0300

    rename contacts to external contacts

--- app/controllers/external_contacts_controller.rb
+++ app/controllers/external_contacts_controller.rb
@@ -1,11 +1,11 @@
-class ContactsController < UserApplicationController
+class ExternalContactsController < UserApplicationController
   layout 'popup'
   skip_before_filter :verify_authenticity_token, :only => :create
   def index
     if params[:provider].present?
       provider = params[:provider].to_sym
       consumer = Contacts.new(provider)
-      url = consumer.authentication_url(url_for :controller => 'contacts', :action => 'index')
+      url = consumer.authentication_url(url_for :controller => 'external_contacts', :action => 'index')
       session[:consumer] = consumer.serialize
       session[:provider] = provider
       redirect_to url
@@ -13,7 +13,7 @@ class ContactsController < UserApplicationController
       provider = session[:provider]
       consumer = Contacts.deserialize_consumer(provider, session[:consumer])
       if consumer.authorize(params)
-        Rails.logger.debug 'consumer authorized, listing contacts'
+        Rails.logger.debug 'consumer authorized, listing external_contacts'
         @contacts = consumer.contacts
       else
         # handle error
