1207f1a253fc8a991448b419a7e859f6cfd660ac
Luis Perichon <luigi@luigi-laptop.(none)>
2010-08-30 15:03:52 -0300

    resend invitations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,8 +2,7 @@ class ContactsController < UserApplicationController
   # GET /contacts
   # GET /contacts.xml
   def index
-    @contacts = current_user.contacts.paginate :page => params[:page], :per_page => 9
-
+    @contacts = current_user.contacts.paginate :page => params[:page], :per_page => 5
     respond_to do |format|
       format.html { } # index.html.erb
       format.xml  { render :xml => @contacts }
