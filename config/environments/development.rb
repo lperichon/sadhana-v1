634aaf4002cdacf321a6e13b20830ddf16c01a39
Luis Perichon <luigi@luigi-laptop.(none)>
2010-10-05 11:13:40 -0300

    stable rails3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,6 @@ Sadhana::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'local.sadhana.com.ar' }
+
+  config.active_support.deprecation =  :log
 end
