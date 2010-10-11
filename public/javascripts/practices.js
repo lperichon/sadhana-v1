8083fc66ea5f3cc6df8917815e3a4bcb3f59e82f
Luis Perichon <luigi@luigi-laptop.(none)>
2010-10-11 12:18:26 -0300

    update state_machine gem. Fixes bug on signup: Users::RegistrationsController# (NoMethodError) "undefined method `remove_attributes_protected_from_mass_assignment' for #<Subscription:0x2ac40ebfa778>"

--- public/javascripts/practices.js
+++ public/javascripts/practices.js
@@ -70,7 +70,7 @@ var Practices = {
   },
   calculateEditorHeight: function() {
     ff_diff = $.browser.mozilla ? 2 : 0;
-    return $(window).height() - $('#practice_data').height() - $('#header').height() - $('#practice_actions').height() - $('#notifications').height() - $('#tips').height() - 140 + ff_diff;
+    return $(window).height() - $('#practice_data').height() - $('#header').height() - $('#practice_actions').height() - $('#notifications').height() - $('#tips').height() - 131 + ff_diff;
   },
   init_quicksearch: function() {
     Practices.qs = $('#q').quicksearch('.techniques-list .technique', {selector: 'h3'});
