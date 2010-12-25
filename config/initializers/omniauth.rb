3b0f8ec481d180510756e29d5f99e557a9e412a3
Luis Perichon <luisperichon+github@gmail.com>
2010-12-25 18:09:53 -0300

    twitter oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, 'rlIKIFp98svCqZ07CtyMg', 'RNeXKTP7RNM0oZ5K8anz1IXlJAx4qpdlLxW1qk4XI'
   provider :facebook, '188062824541861', '2fb1213831287b92cd7762b122387696'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
