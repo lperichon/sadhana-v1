94d6376354a1059972ce99727f82a20e847ca23f
Luis Perichon <luisperichon+github@gmail.com>
2010-12-26 17:24:25 -0300

    translations

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,17 +7,17 @@ class AuthenticationsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
-      flash[:notice] = "Signed in successfully."
+      flash[:notice] = t('authentications.create.signin_successful')
       sign_in_and_redirect(:user, authentication.user)
     elsif current_user
       current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      flash[:notice] = "Authentication successful."
+      flash[:notice] = t('authentications.create.authentication_successful')
       redirect_to :back
     else
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
-        flash[:notice] = "Signed in successfully."
+        flash[:notice] = t('authentications.create.signin_successful')
         sign_in_and_redirect(:user, user)
       else
         session[:omniauth] = omniauth.except('extra')
@@ -29,7 +29,7 @@ class AuthenticationsController < ApplicationController
   def destroy
     @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
-    flash[:notice] = "Successfully destroyed authentication."
+    flash[:notice] = t('authentications.destroy.notice')
     redirect_to :back
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'rlIKIFp98svCqZ07CtyMg', 'RNeXKTP7RNM0oZ5K8anz1IXlJAx4qpdlLxW1qk4XI'
   provider :facebook, '188062824541861', '2fb1213831287b92cd7762b122387696'
+  #provider :open_id, OpenID::Store::Memcache.new(Dalli::Client.new)
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
