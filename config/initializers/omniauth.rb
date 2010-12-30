27b5d96658d2a548b44d03d9a48bd3b59f0afcc7
Luis Perichon <luisperichon+github@gmail.com>
2010-12-30 11:17:32 -0300

    load fb and twitter keys from env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,20 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'rlIKIFp98svCqZ07CtyMg', 'RNeXKTP7RNM0oZ5K8anz1IXlJAx4qpdlLxW1qk4XI'
-  provider :facebook, '188062824541861', '2fb1213831287b92cd7762b122387696', :scope => 'offline_access, publish_stream, email'
+  if Rails.env == 'development'
+    config = YAML.load_file("#{Rails.root}/config/omniauth.yml")
+  else
+    config = {
+      :twitter => {
+        :consumer_key => ENV['TWITTER_CONSUMER_KEY'],
+        :consumer_secret => ENV['TWITTER_CONSUMER_SECRET']
+      },
+      :facebook => {
+        :app_id => ENV['FACEBOOK_APP_ID'],
+        :app_secret => ENV['FACEBOOK_APP_SECRET']
+      }
+    }
+  end
+  provider :twitter, config['twitter']['consumer_key'], config['twitter']['consumer_secret']
+  provider :facebook, config['facebook']['app_id'], config['facebook']['app_secret'], :scope => 'offline_access, publish_stream, email'
   #provider :open_id, OpenID::Store::Memcache.new(Dalli::Client.new)
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
