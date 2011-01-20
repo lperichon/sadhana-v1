0e4a1eac24bf0bab4b053047aa386adf3e11d3e2
Luis Perichon <luisperichon+github@gmail.com>
2011-01-19 21:36:22 -0300

    back to fbml multi friend selector

--- app/controllers/facebook_contacts_controller.rb
+++ app/controllers/facebook_contacts_controller.rb
@@ -1,7 +1,13 @@
 class FacebookContactsController < UserApplicationController
   include Facebooker2::Rails::Controller
   layout 'popup'
+  skip_before_filter :verify_authenticity_token, :only => :create
 
   def index
+  end
+
+  def create
+    flash[:notice] = t('facebook_contacts.create.success')
+    redirect_to facebook_contacts_path
   end
 end
