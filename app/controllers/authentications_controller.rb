98f5961760abd89f1333151e371321d9356766a2
Luis Perichon <luisperichon+github@gmail.com>
2011-01-28 12:03:25 -0300

    a logged in user can accept an invitation

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,13 +7,27 @@ class AuthenticationsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
+      # when there is an authentication for that provider and uid
       flash[:notice] = t('authentications.create.signin_successful')
+      # link contacts
+      if(session["invited-by"])
+        user = User.find(session["invited-by"])
+        authentication.user.add_to_contacts(user, true, true)
+      end
+      # sign in automatically
       sign_in_and_redirect(:user, authentication.user)
     elsif current_user
+      # else if there is a signed in user create the authentication
       current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'], :token =>(omniauth['credentials']['token'] rescue nil))
+      # and link contacts
+      if(session["invited-by"])
+        user = User.find(session["invited-by"])
+        current_user.add_to_contacts(user, true, true)
+      end
       flash[:notice] = t('authentications.create.authentication_successful')
       redirect_to practices_path
     else
+      # else create a new user with the authentication and the contact
       user = User.new(:invited_by => session["invited-by"])
       user.apply_omniauth(omniauth)
       if user.save
