509b676c4fd5a298bf2380ad766a3f5aa2da664c
Luis Perichon <luisperichon+github@gmail.com>
2011-06-09 16:36:47 -0300

    admin users

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -1,3 +1,39 @@
 ActiveAdmin.register Subscription do
   menu false
+
+  form do |f|
+    f.inputs "Subscription Details" do
+      f.input :plan, :input_html => {:name => 'plan'}
+      f.input :balance_cents, :input_html => {:name => 'cents'}
+    end
+    f.buttons
+  end
+
+  controller do
+    def update
+      # find the plan
+      plan = SubscriptionPlan.find params[:plan]
+
+      if plan.nil?
+        flash[:notice] = "Plan not available"
+
+      # perform the change using #change_plan, dont just assign it
+      elsif resource.change_plan(plan)
+        # after change_plan, call renew
+        case result = resource.renew
+        when false
+          flash[:notice] << "An error occured trying to charge the credit card. Please update card information."
+        when Money
+          flash[:notice] << "The credit card has been charged #{result.format(:symbol => true)}"
+        end
+      end
+
+      cents = params[:cents]
+      @subscription.balance_cents = cents
+
+      update! do |success, failure|
+        failure.html { render_or_default 'edit' }
+      end
+    end
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,10 +1,20 @@
 ActiveAdmin.register User do
+
+  SubscriptionPlan.all.each do |plan|
+    scope "#{plan.name} Users" do |users|
+      users.joins(:subscription).where(:subscriptions => {:plan_id => plan.id})
+    end
+  end
+
   index do
     column :email
-    column :current_sign_in_at
-    column :last_sign_in_at
-    column :sign_in_count
+    column :last_sign_in_at, :sortable => :last_sign_in_at  do |user|
+      distance_of_time_in_words_to_now(user.last_sign_in_at) unless user.last_sign_in_at.nil?
+    end
     default_actions
+    column do |user|
+      link_to icon(:arrow_right_alt1) + "Sign In", sign_in_as_path(:id => user.id), :class => "sign_in_link", :method => :post
+    end
   end
 
   form do |f|
