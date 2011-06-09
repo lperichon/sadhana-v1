2c504cdf8fee3564018e8a05c6e9456133c0dd37
Luis Perichon <luisperichon+github@gmail.com>
2011-06-09 13:53:48 -0300

    Remove hoptoad

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,4 +57,3 @@ namespace :deploy do
   end
 end
         require 'config/boot'
-        require 'hoptoad_notifier/capistrano'
--- config/initializers/hoptoad.rb
+++
@@ -1,9 +0,0 @@
-HoptoadNotifier.configure do |config|
-  if Rails.env == 'development'
-    dev_config = YAML.load_file("#{Rails.root}/config/hoptoad.yml")
-    config.api_key = dev_config['api_key']
-  else
-    config.api_key = ENV['HOPTOAD_API_KEY']
-  end
-  config.js_notifier = true
-end
