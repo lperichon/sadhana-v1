54d2d6dee12f8aeef3603f3041a9194bd5df8435
Luis Perichon <luisperichon+github@gmail.com>
2011-06-09 17:33:28 -0300

    better use of active_admin dsl

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -9,31 +9,17 @@ ActiveAdmin.register Subscription do
     f.buttons
   end
 
-  controller do
-    def update
-      # find the plan
-      plan = SubscriptionPlan.find params[:plan]
+  before_update do
+    # find the plan
+    plan = SubscriptionPlan.find params[:plan]
 
-      if plan.nil?
-        flash[:notice] = "Plan not available"
-
-      # perform the change using #change_plan, dont just assign it
-      elsif resource.change_plan(plan)
-        # after change_plan, call renew
-        case result = resource.renew
-        when false
-          flash[:notice] << "An error occured trying to charge the credit card. Please update card information."
-        when Money
-          flash[:notice] << "The credit card has been charged #{result.format(:symbol => true)}"
-        end
-      end
-
-      cents = params[:cents]
-      @subscription.balance_cents = cents
-
-      update! do |success, failure|
-        failure.html { render_or_default 'edit' }
-      end
+    # perform the change using #change_plan, dont just assign it
+    if plan && resource.change_plan(plan)
+      # after change_plan, call renew
+      resource.renew
     end
+
+    cents = params[:cents]
+    @subscription.balance_cents = cents
   end
 end
