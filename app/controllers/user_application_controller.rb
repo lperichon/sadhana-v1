f3380d23a6c90d85a59b9ff8ac4fde372e31f5ec
Luis Perichon <luisperichon+github@gmail.com>
2011-06-16 13:13:04 -0300

    only track user actions

--- app/controllers/user_application_controller.rb
+++ app/controllers/user_application_controller.rb
@@ -1,3 +1,5 @@
 class UserApplicationController < ApplicationController
   before_filter :authenticate_user!
+
+  analytical :modules=>[:console, :google, :kiss_metrics], :use_session_store=>true
 end
