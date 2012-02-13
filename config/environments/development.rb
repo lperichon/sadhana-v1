9c65c4c1deee33f2c2344842bad773c27a50fe8d
Luis Perichon <luisperichon+github@gmail.com>
2012-02-13 17:21:47 -0300

    environments config for rails 3.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Sadhana::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -20,4 +20,10 @@ Sadhana::Application.configure do
   config.action_mailer.default_url_options = { :host => 'local.sadhana.com.ar' }
 
   config.active_support.deprecation =  :log
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,4 +28,11 @@ Sadhana::Application.configure do
   # config.active_record.schema_format = :sql
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
+
+  # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
+  config.assets.allow_debugging = true
 end
