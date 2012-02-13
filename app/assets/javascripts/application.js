5c8d7b37c3d9d72678d1bb4150427ad5f1b91e30
Luis Perichon <luisperichon+github@gmail.com>
2012-02-13 17:21:07 -0300

    move to assets

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require ui.selectmenu
+//= require app
+//= require jquery.jeditable
+//= require jquery.cycle
+//= require jquery.countdown
+//= require jquery.jplayer.min
+//= require fullcalendar
+//= require timepicker
+//= require jquery.quicksearch
+//= require jquery.tweet
+//= require jquery.jstore-all-min
+//= require css_browser_selector
