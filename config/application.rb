8b12cd70677f3f8cb97ee19efa3eb6c8475e78f0
Luis Perichon <luisperichon@gmail.com>
2012-08-18 21:20:33 -0300

    fix language select

--- config/application.rb
+++ config/application.rb
@@ -67,5 +67,7 @@ module Sadhana
 
     # Cookie secret (Originaly from initializers/cookie_verification_secret.rb)
     config.secret_token = '425a9b6ff5b1cec8e884220b1c1521a945a069d14f50e08799104891d08c5012584043a3f98038c62af45dc5a12cb42f5a5e6d78be16217e288d74986c713fa8'
+
+    config.i18n.available_locales = [:en, :es, :pt]
   end
 end
