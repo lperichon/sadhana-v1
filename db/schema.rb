d08b60a28265101bf148f83e72b2003716c559c4
Luis Perichon <luisperichon@gmail.com>
2012-11-07 20:57:03 -0300

    Add reset password sent at column to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424190536) do
+ActiveRecord::Schema.define(:version => 20120820060719) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -265,7 +266,10 @@ ActiveRecord::Schema.define(:version => 20130424190536) do
     t.text     "google_consumer"
     t.text     "yahoo_consumer"
     t.datetime "reset_password_sent_at"
+<<<<<<< HEAD
     t.integer  "invited_by_id"
+=======
+>>>>>>> Add reset password sent at column to users
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
