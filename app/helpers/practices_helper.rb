0b3b504af6f06d34e488b0cca30185832a0de38a
Luis Perichon <luisperichon@gmail.com>
2012-11-07 21:26:11 -0300

    fix practices helper

--- app/helpers/practices_helper.rb
+++ app/helpers/practices_helper.rb
@@ -1,8 +1,8 @@
 module PracticesHelper
   def first_part(practice)
     if current_user.subscription_plan == SubscriptionPlan.default_plan
-      result = practice.practice_parts.select {|pp| pp.part.symbol == 'asana'}
-      first_part = result.first.part unless result.empty?
+      result = practice.practice_parts.find {|pp| pp.part.symbol == 'asana'}
+      first_part = result.part unless result.blank?
     else
       first_part = practice.practice_parts.first.part unless practice.practice_parts.empty?
     end
@@ -12,10 +12,9 @@ module PracticesHelper
 
   def first_part_index(practice)
     if current_user.subscription_plan == SubscriptionPlan.default_plan
-      result = practice.practice_parts.select {|pp| pp.part.symbol == 'asana'}
+      first_part = practice.practice_parts.find {|pp| pp.part.symbol == 'asana'}
       first_part_index = 0
-      unless result.empty?
-        first_part = result.first
+      unless first_part.blank?
         first_part_index = practice.practice_parts.index first_part
       end
     else
@@ -27,10 +26,9 @@ module PracticesHelper
 
   def disabled_parts(practice)
     if current_user.subscription_plan == SubscriptionPlan.default_plan
-      result = practice.practice_parts.select {|pp| pp.part.symbol == 'asana'}
+      first_part = practice.practice_parts.find {|pp| pp.part.symbol == 'asana'}
       first_part_index = 0
-      unless result.empty?
-        first_part = result.first
+      unless first_part.blank?
         first_part_index = practice.practice_parts.index first_part
         disabled_parts = (0..practice.practice_parts.count).to_a.reject {|i| i == first_part_index}
       end
